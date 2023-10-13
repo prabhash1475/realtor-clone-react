cb27b048da9caeed7bc5eaf35d549dea60b3c4b6
Prabhash Kumar <prabhash.kumar96@gmail.com>
2023-10-14 01:31:23 +0530

    created the header components

---
+++ src/components/Header.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+export default function Header() {
+  const location = useLocation();
+  //   location.pathname  will give path of current page
+  //   console.log(location);
+
+  const navigate = useNavigate();
+  console.log(navigate);
+
+  function pathMatchRoute(route) {
+    if (route === location.pathname) {
+      return true;
+    }
+  }
+
+  return (
+    <div className="bg-white border-b shadow-sm static top-0 z-50">
+      <header className="flex justify-between px-3 items-center mx-auto max-w-6xl">
+        <div>
+          <img
+            className="h-5 cursor-pointer"
+            src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
+            alt="logo"
+            onClick={() => navigate("/")}
+          />
+        </div>
+        <div>
+          <ul className="flex space-x-10">
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/")}
+            >
+              Home
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/offers") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/offers")}
+            >
+              Offers
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/sign-in") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/sign-in")}
+            >
+              Sign in
+            </li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+}
